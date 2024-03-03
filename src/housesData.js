fb57313b04b1fc7ed0c826e6eee72b113beefbec
Friedrich482 <friedricht109@gmail.com>
2024-03-03 23:46:09 +0100

    added ✅ the houses to the UI

---
+++ src/housesData.js
@@ -0,0 +1,36 @@
+import React from "react";
+import House1 from "./assets/imgs/houses/h1.jpg";
+import House2 from "./assets/imgs/houses/h2.jpeg";
+import House3 from "./assets/imgs/houses/h3.jpg";
+import House4 from "./assets/imgs/houses/h4.jpg";
+import House5 from "./assets/imgs/houses/h5.jpeg";
+const houses = [
+  {
+    src: House1,
+    description:
+      "A wonderful house at Kansas City. Much beautiful the night with a lot of light! Enjoy your house! ",
+    price: 120000
+  },
+  {
+    src: House2,
+    description: "The house of your dreams with a pool. Perfect for your holidays !",
+    price: 200000
+  },
+  {
+    src: House3,
+    description: "A little castle perfect for Chrismas. Santa will bring you beautiful presents !",
+    price: 115000
+  },
+  {
+    src: House4,
+    description: "A fortress with a big park. Your kids wont miss place to play. Perfect for a short seclusion !",
+    price: 150000
+  },
+  {
+    src: House5,
+    description: "That house is your dream place if you want to stay cozy and have park and forest near you! Enjoy your palace ! ",
+    price: 99000
+  },
+];
+
+export default houses

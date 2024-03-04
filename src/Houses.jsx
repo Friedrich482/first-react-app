606e187f3c7376559923fe76d92d323a5dd33c11
Friedrich482 <friedricht109@gmail.com>
2024-03-04 01:46:46 +0100

    added a small useState effect on the buy 💵 button

--- src/Houses.jsx
+++ src/Houses.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import houses from "./housesData";
+import { useState } from "react";
 const Houses = () => {
+  const [buyButton, setBuyButton] = useState("Buy Now !");
   return houses.map((house, index) => (
     <div
       key={index}
-      className=" flex items-center flex-col border-8 border-teal-700 rounded-lg min-w-[450px] sm:w-10/12 gap-2 h-[640px]"
+      className=" flex items-center flex-col border-8 border-teal-700 rounded-lg min-w-[450px] sm:w-10/12 gap-2 h-[650px]"
     >
       <img
         src={house.src}
         alt={`House n°${index + 1}`}
         className="size-96 aspect-square mt-8 border-solid border-4 border-red-500 rounded-lg"
       ></img>
-      <p className="text-center text-yellow-500 w-5/6 text-xl">{house.description}</p>
-      <p className="text-3xl text-black font-bold bg-red-600 w-full border-yellow-300 border-double border-t-4 border-b-4 text-center">{house.price} $</p>
-      <button type="button" className="bg-yellow-600 hover:scale-110 transition duration-1000 hover:border-white hover:border-2 hover:text-xl hover:text-black">Buy Now !</button>
+      <p className="text-center text-yellow-500 w-5/6 text-xl">
+        {house.description}
+      </p>
+      <p className="text-3xl text-black font-bold bg-red-600 w-full border-yellow-300 border-double border-t-4 border-b-4 text-center">
+        {house.price} $
+      </p>
+      <button
+        type="button"
+        className="bg-yellow-600 hover:scale-110 transition duration-1000 hover:border-white hover:border-2 hover:text-xl hover:text-black"
+        onClick={(event) => {
+          event.target.innerText === "Buy Now !"
+            ? setBuyButton("Sold !")
+            : setBuyButton("Buy Now !");
+        }}
+      >
+        {buyButton}
+      </button>
     </div>
   ));
 };

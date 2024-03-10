47d55c2e134ba444725e92e8982b1828b2f31f5b
Friedrich482 <friedricht109@gmail.com>
2024-03-10 23:29:01 +0100

    ✅ added new houses to the app

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,9 +3,9 @@ import { Icons } from "./Icons";
 const Footer = ({iconsProps}) => {
   const date = new Date();
   return (
-    <footer className="flex items-center justify-center flex-col text-2xl text-black bg-stone-300 border-t-4 border-t-black">
+    <footer className="flex items-center justify-center flex-col text-2xl text-black bg-stone-300 border-t-4 border-t-black text-center">
       <p>&copy; {date.getFullYear()}, Friedrich482, All rights reserved</p>
-      <div className="flex flex-row gap-4 mb-3">
+      <div className="flex flex-row gap-2 mb-3">
         <Icons iconsProps={iconsProps}/>
       </div>
     </footer>
--- src/data/housesData.js
+++ src/data/housesData.js
@@ -5,7 +5,17 @@ import House3 from "../assets/imgs/houses/h3.avif";
 import House4 from "../assets/imgs/houses/h4.avif";
 import House5 from "../assets/imgs/houses/h5.avif";
 import House6 from "../assets/imgs/houses/h6.avif";
-const houses = [
+import House7 from "../assets/imgs/houses/h7.avif";
+import House8 from "../assets/imgs/houses/h8.avif";
+import House9 from "../assets/imgs/houses/h9.avif";
+import House10 from "../assets/imgs/houses/h10.avif";
+import House11 from "../assets/imgs/houses/h11.avif";
+import House12 from "../assets/imgs/houses/h12.avif";
+import House13 from "../assets/imgs/houses/h13.avif";
+import House14 from "../assets/imgs/houses/h14.avif";
+import House15 from "../assets/imgs/houses/h15.avif";
+
+const houses1 = [
   {
     id: 1,
     src: House1,
@@ -53,5 +63,78 @@ const houses = [
     sold: false,
   },
 ];
+const houses2 = [
+  {
+    id: 7,
+    src: House7,
+    description:
+      "Kansas City House. Much beautiful the night with a lot of light !",
+    price: 120000,
+    sold: true,
+  },
+  {
+    id: 8,
+    src: House8,
+    description:
+      "The house of your dreams with a pool. Perfect for your holidays !",
+    price: 200000,
+    sold: false,
+  },
+  {
+    id: 9,
+    src: House9,
+    description: "With this one and its beautiful pool, the summer is yours !",
+    price: 115000,
+    sold: false,
+  },
+  {
+    id: 10,
+    src: House10,
+    description:
+      "A fortress with a big park. Your kids wont miss space to play !",
+    price: 150000,
+    sold: true,
+  },
+  {
+    id: 11,
+    src: House11,
+    description:
+      "This house is perfect if you want to stay cozy and have forest near you !",
+    price: 99000,
+    sold: false,
+  },
+  {
+    id: 12,
+    src: House12,
+    description: "A simple ward for those who prefer little towns!",
+    price: 99000,
+    sold: false,
+  },
+];
+const houses3 = [
+  {
+    id: 13,
+    src: House13,
+    description:
+      "Kansas City House. Much beautiful the night with a lot of light !",
+    price: 120000,
+    sold: true,
+  },
+  {
+    id: 14,
+    src: House14,
+    description:
+      "The house of your dreams with a pool. Perfect for your holidays !",
+    price: 200000,
+    sold: false,
+  },
+  {
+    id: 15,
+    src: House15,
+    description: "With this one and its beautiful pool, the summer is yours !",
+    price: 115000,
+    sold: false,
+  },
+];
 
-export default houses;
+export default {houses1, houses2, houses3};

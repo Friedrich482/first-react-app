5732cc7c8678920cff55c514a625595a36657889
Friedrich482 <friedricht109@gmail.com>
2024-03-28 00:46:36 +0100

    ✅ replaced avif images by webp images

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,7 @@
 import Houses from "./Houses";
 const Content = ({ houses, badge, houseSold, setHouseSold, handleClick }) => {
   return (
-    <main className="flex w-full flex-wrap items-center justify-center gap-10 pl-3 pr-3">
+    <main className="flex w-full flex-wrap items-center justify-center gap-10 pl-10 pr-10">
       <Houses
         houses={houses}
         badge={badge}

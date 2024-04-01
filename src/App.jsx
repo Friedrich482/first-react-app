1560ffb561bdc9ce57d4f6c9fca71dc1ca8378a8
Friedrich482 <friedricht109@gmail.com>
2024-04-01 02:40:19 +0200

    ✅ added additional style when the house is sold

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,6 @@ import Footer from "./components/Footer";
 import "./index.css";
 
 function App() {
-  // const [houseSold, setHouseSold] = useState(Array(houses.length).fill(false));
-  // const handleClick = (index) => {
-  //   const newHouseSold = [...houseSold];
-  //   newHouseSold[index] = true;
-  //   setHouseSold(newHouseSold);
-  // };
   return (
     <div className="align-center flex flex-col justify-center gap-12">
       <Header />
